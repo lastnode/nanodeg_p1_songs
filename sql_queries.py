cd229c8ba5bdee99ddb09616622b8c28d137cece
Mahangu <mahangu@automattic.com>
2020-06-04 18:31:18 +0530

    Wrote queries for artists table and started work on processing .

--- sql_queries.py
+++ sql_queries.py
@@ -3,7 +3,7 @@
 songplay_table_drop = ""
 user_table_drop = ""
 song_table_drop = "DROP TABLE IF EXISTS songs"
-artist_table_drop = ""
+artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = ""
 
 # CREATE TABLES
@@ -23,7 +23,13 @@ duration int,
 PRIMARY KEY (song_id))
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE artists (
+artist_id text,
+artist_name text,
+artist_location text, 
+artist_latitude double precision, 
+artist_longitude double precision, 
+PRIMARY KEY (artist_id))
 """)
 
 time_table_create = ("""
@@ -44,6 +50,9 @@ INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s
 """)
 
 artist_table_insert = ("""
+
+INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING
+
 """)
 
 
@@ -56,10 +65,11 @@ song_select = ("""
 """)
 
 # QUERY LISTS
+# Only enabling the queries we have completed so far.
 
-create_table_queries = [song_table_create]
+create_table_queries = [song_table_create,artist_table_create]
 
-drop_table_queries = [song_table_drop]
+drop_table_queries = [song_table_drop,artist_table_drop]
 
 # create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 # drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
