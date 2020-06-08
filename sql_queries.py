17df7de8a93e1502724a5358aaf73b8bd4bef94b
Mahangu <mahangu@automattic.com>
2020-06-08 17:44:03 +0530

    Changed queries to ON CONFLICT() DO UPDATE SET.

--- sql_queries.py
+++ sql_queries.py
@@ -63,32 +63,31 @@ PRIMARY KEY (ts))
 
 songplay_table_insert = ("""
 
-INSERT INTO songplays (ts, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (songplay_id) DO NOTHING
+INSERT INTO songplays (ts, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (songplay_id) DO UPDATE SET ts=EXCLUDED.ts, user_id=EXCLUDED.user_id, level=EXCLUDED.level, song_id=EXCLUDED.song_id, artist_id = EXCLUDED.artist_id, session_id=EXCLUDED.session_id, location=EXCLUDED.location, user_agent=EXCLUDED.user_agent
 
 """)
 
 user_table_insert = ("""
 
-INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING
+INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET first_name=EXCLUDED.first_name, last_name=EXCLUDED.last_name, gender=EXCLUDED.gender, level=EXCLUDED.level 
 
 """)
 
 song_table_insert = ("""
 
-INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING
-
+INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO UPDATE SET title=EXCLUDED.title,  artist_id=EXCLUDED.artist_id, year=EXCLUDED.year, duration=EXCLUDED.duration
 """)
 
 artist_table_insert = ("""
 
-INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING
+INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO UPDATE SET artist_id=EXCLUDED.artist_id, artist_name=EXCLUDED.artist_name, artist_location=EXCLUDED.artist_location, artist_latitude=EXCLUDED.artist_latitude, artist_longitude=EXCLUDED.artist_longitude
 
 """)
 
 
 time_table_insert = ("""
 
-INSERT INTO time (ts, hour, day, week_of_year, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (ts) DO NOTHING
+INSERT INTO time (ts, hour, day, week_of_year, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (ts) DO UPDATE SET hour=EXCLUDED.hour, day=EXCLUDED.day, week_of_year=EXCLUDED.week_of_year, month=EXCLUDED.month, year=EXCLUDED.year, weekday=EXCLUDED.weekday 
 
 """)
 
@@ -105,8 +104,6 @@ WHERE songs.title = %s and
 """)
 
 # QUERY LISTS
-# Only enabling the queries we have completed so far.
-
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
